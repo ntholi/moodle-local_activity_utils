8048d29cadb09aefcf01fd05d347ea81607eb1fe
Ntholi Nkhatho <ntholi@live.com>
2025-11-27 15:52:15 +0200

    Add script for local_createassign plugin

---
+++ build.bat
@@ -0,0 +1,52 @@
+@echo off
+REM Build script for local_createassign Moodle plugin
+REM This creates an installable ZIP package
+
+setlocal
+
+REM Set variables
+set PLUGIN_NAME=local_createassign
+set BUILD_DIR=build
+set PACKAGE_DIR=%BUILD_DIR%\%PLUGIN_NAME%
+set OUTPUT_FILE=%BUILD_DIR%\%PLUGIN_NAME%.zip
+
+echo Building %PLUGIN_NAME% plugin...
+echo.
+
+REM Clean up previous build (clear entire build folder)
+if exist %BUILD_DIR% (
+    echo Cleaning up previous build...
+    rmdir /s /q %BUILD_DIR%
+)
+
+REM Create build directory structure
+echo Creating build directory...
+mkdir %PACKAGE_DIR%
+
+REM Copy plugin files
+echo Copying plugin files...
+xcopy /E /I /Y classes %PACKAGE_DIR%\classes
+xcopy /E /I /Y db %PACKAGE_DIR%\db
+xcopy /E /I /Y lang %PACKAGE_DIR%\lang
+copy /Y version.php %PACKAGE_DIR%\
+copy /Y README.md %PACKAGE_DIR%\
+
+REM Create ZIP archive using PowerShell
+echo Creating ZIP archive...
+powershell -Command "Compress-Archive -Path '%PACKAGE_DIR%\*' -DestinationPath '%OUTPUT_FILE%' -Force"
+
+REM Clean up temporary package directory
+echo Cleaning up...
+rmdir /s /q %PACKAGE_DIR%
+
+echo.
+echo Build complete!
+echo Package created: %OUTPUT_FILE%
+echo.
+echo You can now install this plugin in Moodle:
+echo 1. Go to Site administration ^> Plugins ^> Install plugins
+echo 2. Upload the ZIP file from the build folder
+echo 3. Follow the installation wizard
+echo.
+
+endlocal
